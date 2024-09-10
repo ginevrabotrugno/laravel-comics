b16730a0b2fd83b7c3cbba6c8d0f15db55ac632c
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-10 18:59:19 +0200

    style: change about and contact content

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,23 +5,9 @@
 
 <section class="content">
     <div class="container">
-        <div class="row">
+        <h1>CONTACT</h1>
 
-            @foreach ( $cards as $card )
-
-            <div class="card">
-
-                <div class="thumb">
-                    <img src=" {{ $card['thumb'] }} " alt="{{ $card['title'] }}">
-                </div>
-                <h6> {{ $card['title'] }} </h6>
-
-            </div>
-
-            @endforeach
-
-
-        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti consequuntur porro perspiciatis incidunt, molestias consectetur quidem ea? Laborum maxime atque sunt accusantium. Voluptatum dignissimos assumenda repudiandae alias, ut voluptatibus culpa ea ipsam amet ullam praesentium aspernatur saepe quisquam ipsum cupiditate soluta quam ex, similique molestias odit rem vitae temporibus, quos blanditiis. Suscipit fugiat, doloribus et odit pariatur tempore deleniti voluptas accusamus accusantium, eos, consequatur commodi? Nulla esse dicta autem reprehenderit iure, quod nam unde voluptatem eaque mollitia laudantium sapiente, cumque accusantium placeat, nobis laboriosam. Beatae dolorum culpa delectus voluptate iusto soluta, accusantium quaerat dolores, magnam ipsam dignissimos placeat recusandae commodi quia hic vero molestias quibusdam possimus. Quod dolorum impedit explicabo dolore deserunt facere, autem error exercitationem architecto ex minima. Quam, animi. Cupiditate, voluptatum enim modi fugiat quaerat vel vitae assumenda itaque ipsa commodi perspiciatis impedit mollitia ipsam repudiandae tempora, aperiam dignissimos eum nostrum quos necessitatibus cumque odio magnam? Dolorem quam reprehenderit et nulla, corporis sequi optio fugiat non harum doloremque ipsum totam iste officia tempore cumque sed dolore, omnis ab. Repudiandae non deleniti iste maiores voluptatum tenetur natus earum, esse itaque doloribus dolores eos vero labore fuga sed nam enim corporis? Porro illum, dicta facilis at perspiciatis laudantium aperiam dignissimos.</p>
 
         <a href="#" class="button">
             LOAD MORE
